54a7a1ca7d038486f4154ce32cd70bda91714fdf
Katherine Nguyen <khnguyen86@live.com>
2020-12-30 15:03:16 -0800

    new ability class

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    @user = user
+    @user ||= User.new # guest user (not logged in)
+    can :read, :all
+
+    if @user.admin?
+      can :manage, :all
+    else
+      can [:update, :destroy], Recipe, user_id: @user.id
+    end
+  end
+end
