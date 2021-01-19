a768a47a0a7a36bd7bb85f73cc0b21a707196015
Katherine Nguyen <khnguyen86@live.com>
2021-01-19 14:25:12 -0800

    adds new user welcome modal to upload profile photo

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
   # GET /
-  def index; end
+  def index
+    @user = current_user
+    return unless @user.sign_in_count == 1 && !@user.admin?
+
+    session[:new_user_modal] = true
+  end
 end
