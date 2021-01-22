fe77b9dc36ae7cc8c932ea8a82502508ee0dd48f
Katherine Nguyen <khnguyen86@live.com>
2021-01-22 13:41:41 -0800

    adds specs for new_message_posted user mailer

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Message, type: :model do
     it { is_expected.to validate_presence_of(:subject) }
     it { is_expected.to validate_presence_of(:body) }
   end
+
+  it 'sends non-admin/approved users new_message_posted email after create' do
+    # Assume
+    mailers_before = ActionMailer::Base.deliveries.count
+    user = create(:user)
+    expect(mailers_before).to be(0)
+    non_admin_approved_users_count = User.non_admin_approved_users.count
+
+    # Act
+    Message.create(subject: 'New message', body: 'body', user_id: user.id)
+
+    # Assert
+    mailers_after = ActionMailer::Base.deliveries.count
+    expect(mailers_after).to be(non_admin_approved_users_count)
+  end
 end
