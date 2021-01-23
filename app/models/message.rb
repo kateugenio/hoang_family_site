186c41417498ac4426a75eb76dfaae064d1c58a9
Katherine Nguyen <khnguyen86@live.com>
2021-01-22 17:50:10 -0800

    adds scope method for users that allow new message posted notifications, updates strong params, updates method to send emails to users after message create

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,11 +4,13 @@ class Message < ApplicationRecord
 
   validates :subject, :body, presence: true
 
-  after_create :send_approved_users_notification_email
+  after_create :send_new_message_posted_email
 
-  def send_approved_users_notification_email
+  def send_new_message_posted_email
     # TODO: Use sidekiq/redis to deliver emails in production
-    User.where.not(id: user_id).non_admin_approved_users.each do |recipient_user|
+    User.where.not(id: user_id)
+              .non_admin_approved_users
+              .enables_new_message_posted_notifications.each do |recipient_user|
       UserMailer.new_message_posted(user_id, recipient_user.id, id).deliver
     end
   end
