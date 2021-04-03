0a2c12c2b8c22ea09334ea297edd33518f4854e4
Katherine Nguyen <khnguyen86@live.com>
2021-04-02 21:12:37 -0700

    adds actions to create, show, and destroy family tree

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :verify_allowed_render_tabs, only: [:update_settings, :update_password]
-  before_action :verify_user_is_admin, only: [:admin_photo_album, :create_admin_photo_album]
+  before_action :verify_user_is_admin, only: [:admin_photo_album, :create_admin_photo_album,
+                                              :admin_family_tree, :create_admin_family_tree]
 
   ALLOWED_RENDER_TABS = [
     'settings_general',
@@ -30,7 +31,7 @@ class UsersController < ApplicationController
 
   # GET /admin_photo_album
   def admin_photo_album
-    @admin_photo_album = PhotoAlbum.find_by(is_admin: true)
+    @admin_photo_album = PhotoAlbum.find_by(is_admin: true, admin_type: 'home_page_carousel')
   end
 
   # POST /create_admin_photo_album
@@ -38,7 +39,8 @@ class UsersController < ApplicationController
   def create_admin_photo_album
     @user = current_user
     @photo_album = @user.photo_albums.new(photo_album_params.merge(name: "ADMIN PHOTO ALBUM",
-                                                                   is_admin: true))
+                                                                   is_admin: true,
+                                                                   admin_type: 'home_page_carousel'))
     if @photo_album.save
       flash[:success] = "Successfully created ADMIN PHOTO ALBUM"
       redirect_to admin_photo_album_path
@@ -50,7 +52,7 @@ class UsersController < ApplicationController
   # rubocop: enable Metrics/AbcSize
 
   def update_admin_photo_album
-    @photo_album = PhotoAlbum.find_by(is_admin: true)
+    @photo_album = PhotoAlbum.find_by(is_admin: true, admin_type: 'home_page_carousel')
 
     if @photo_album.update(photo_album_params)
       flash[:success] = "Successfully updated #{@photo_album.name}"
@@ -62,11 +64,39 @@ class UsersController < ApplicationController
   end
 
   def destroy_photo_from_admin_photo_album
-    @admin_photo_album = PhotoAlbum.find_by(is_admin: true)
+    @admin_photo_album = PhotoAlbum.find_by(is_admin: true, admin_type: 'home_page_carousel')
     @image = @admin_photo_album.images.find(params[:image_id])
     @image.purge
 
     redirect_to admin_photo_album_path
+  end
+
+  # GET /admin_family_tree
+  def admin_family_tree
+    @admin_family_tree = PhotoAlbum.find_by(is_admin: true, admin_type: 'family_tree')
+  end
+
+  # POST /create_admin_photo_album
+  def create_admin_family_tree
+    @user = current_user
+    @family_tree = @user.photo_albums.new(photo_album_params.merge(name: "FAMILY TREE",
+                                                                   is_admin: true,
+                                                                   admin_type: 'family_tree'))
+    if @family_tree.save
+      flash[:success] = "Successfully uploaded a family tree image."
+      redirect_to admin_family_tree_path
+    else
+      flash.now[:error] = @family_tree.errors.messages.values.flatten.uniq
+      render :admin_family_tree
+    end
+  end
+
+  # DELETE /family_tree_photo
+  def destroy_family_tree_photo
+    @family_tree_photo = PhotoAlbum.find_by(is_admin: true, admin_type: 'family_tree')
+    @family_tree_photo.destroy
+
+    redirect_to admin_family_tree_path
   end
 
   # PATCH /users/attach_avatar
